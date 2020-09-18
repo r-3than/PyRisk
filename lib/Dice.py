a0336cdd15f1e3c03a811ede25178f2868a08e27
Ethan Ray <ethan.gregory.ray@gmail.com>
2020-09-18 11:34:45 +0100

    Added a dice handler

--- lib/Dice.py
+++ lib/Dice.py
@@ -1,5 +1,24 @@
 import pygame
 switcher = {1:one,2:two,3:three,4:four,5:five,6:six}
+RED = (200,10,10)
+BLUE = (10,10,200)
+class DiceHandler:
+    def __init__(self,atkVals,defVals,r1,r2):
+        self.atkVals = atkVals #Lists of the dice throws ( already sorted)
+        self.defVals = defVals
+        diff = 30
+        self.Die = []
+        startpoint = ((r1.VisCenter[0]+r2.VisCenter[0])//2,(r1.VisCenter[1]+r2.VisCenter[1])//2)
+        for x in range(0,len(self.atkVals)):
+            aDice = Dice(self.atkVals[x],startpoint[0]+x*diff,startpoint[1],RED)
+            self.Die.append(aDice)
+        for x in range(0,len(self.defVals)):
+            aDice = Dice(self.defVals[x],startpoint[0]+x*diff,startpoint[1]+diff,BLUE)
+            self.Die.append(aDice)
+    def draw(self,scr):
+        for dice in self.Die:
+            dice.draw(scr)
+
 class Dice:
     def __init__(self,value,x1,y1,colour,boxSize=25):
         self.value = value
