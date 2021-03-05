50b0e22e265ecadffae554b074fa75c28c1d5909
r-3than <ethan.gregory.ray@gmail.com>
2021-03-05 14:17:00 +0000

    unit comment

--- lib/Unit.py
+++ lib/Unit.py
@@ -16,7 +16,7 @@ class Unit:
         textRect = self.text.get_rect()
 
         oldpos = self.pos
-        newpos = (oldpos[0]-textRect[2]//2-rad//3.14,oldpos[1]-textRect[3]//2-rad//3.14) #
+        newpos = (oldpos[0]-textRect[2]//2-rad//3.14,oldpos[1]-textRect[3]//2-rad//3.14) #do some pi formual to find goos spot for the text to be in
         # moves to the centre of the circle (minus offset)
         return newpos
     def draw(self,scr):
